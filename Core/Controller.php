74b2a834ebcf1087c086e8e17f209ababbccd537
Janith Madarasinghe <janithpm9991@gmail.com>
2022-11-09 07:08:19 +0530

    feat: redirect funtion in Controller feat: findOne, findAll, findById function in Model

--- Core/Controller.php
+++ Core/Controller.php
@@ -1,30 +1,35 @@
 <?php
 
-    class Controller{
-        var $vars = [];
-        var $layout = false;
+class Controller
+{
+    var $vars = [];
+    var $layout = false;
 
-        function set($d){
-            $this->vars = array_merge($this->vars, $d);
-        }
-
-        function render($filename){
-            extract($this->vars);
-            ob_start();
-            require(ROOT . "Views/" . ucfirst(str_replace('Controller', '', get_class($this))) . '/' . $filename . '.php');
-            $content_for_layout = ob_get_clean();
-            echo($content_for_layout);
-            if($this->layout == false){
-                $content_for_layout;
-            }
-            else{
-                require(ROOT . "Views/Layout/" . $this->layout . '.php');
-            }
-        }
-
-        // private function secure_input($data){
-
-        // }
+    function set($d)
+    {
+        $this->vars = array_merge($this->vars, $d);
     }
 
-?>
+    function render($filename)
+    {
+        extract($this->vars);
+        ob_start();
+        require(ROOT . "Views/" . ucfirst(str_replace('Controller', '', get_class($this))) . '/' . $filename . '.php');
+        $content_for_layout = ob_get_clean();
+        echo ($content_for_layout);
+        if ($this->layout == false) {
+            $content_for_layout;
+        } else {
+            require(ROOT . "Views/Layout/" . $this->layout . '.php');
+        }
+    }
+
+    function redirect($url)
+    {
+        header('Location: ' . URLROOT . $url);
+    }
+
+    // private function secure_input($data){
+
+    // }
+}
