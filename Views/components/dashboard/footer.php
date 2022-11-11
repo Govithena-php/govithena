a7c1bb866560c25ea8b43c3c83c4ab295812eee2
Janith Madarasinghe <janithpm9991@gmail.com>
2022-11-11 16:10:16 +0530

    create investor model, views and controllers

---
+++ Views/components/dashboard/footer.php
@@ -0,0 +1,3 @@
+<div class="footer bg-dark text-light ff-poppins fs-3">
+    GOVITHENA &copy; 2022
+</div>
--- Views/investor/myinvestments.php
+++ Views/investor/myinvestments.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>govithena | dashboard</title>
+    <title>Investor | Dashboard</title>
     <link rel="stylesheet" type="text/css" href="../Webroot/css/sidebar.css">
     <link rel="stylesheet" href="../Webroot/css/ui.css">
     <link rel="stylesheet" href="../Webroot/css/dashHeader.css">
@@ -17,12 +17,10 @@
     <?php include COMPONENTS . 'dashboard/header.php'; ?>
     <?php include 'sidebar.php'; ?>
 
-    <div class="container mt-4">
+    <div class="dashboard__container">
+        <h1>my Investment</h1>
     </div>
-
-
-
-    <!-- <script src="https://kit.fontawesome.com/b8084a92f1.js" crossorigin="anonymous"></script> -->
+    <?php require COMPONENTS . "dashboard/footer.php"; ?>
 </body>
 
 </html>
