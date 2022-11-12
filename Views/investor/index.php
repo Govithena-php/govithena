6865f96893d69a49b08220a5d05caae52c9e812e
Janith Madarasinghe <janithpm9991@gmail.com>
2022-11-12 20:24:59 +0530

    add brand logo and favicon

--- Views/investor/index.php
+++ Views/investor/index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="<?php echo IMAGES ?>/favicon.png">
     <title>govithena | dashboard</title>
     <link rel="stylesheet" type="text/css" href="../Webroot/css/sidebar.css">
     <link rel="stylesheet" href="../Webroot/css/ui.css">
