fbfc05a4bfd96cc36681ccb9e894c1105d3819f4
sanduni <dsanduni@icptechno.com>
2022-11-17 20:29:18 +0530

    feat: customer dashboard sidebar

--- Views/customer/index.php
+++ Views/customer/index.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="../Webroot/css/ui.css">
     <link rel="stylesheet" href="../Webroot/css/dashHeader.css">
     <link rel="stylesheet" href="../Webroot/css/dashFooter.css">
+    <link rel="stylesheet" href="../Webroot/css/dashCustomerOrders.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 </head>
 
