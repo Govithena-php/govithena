17ed4413c3883e7510b1881e209a936a44f284a7
Janith Madarasinghe <janithpm9991@gmail.com>
2022-11-19 18:23:16 +0530

    feat: handle params with or without an action

--- router.php
+++ router.php
@@ -14,14 +14,15 @@ class Router
         } else {
             $explode_url = explode('/', $url);
             $explode_url = array_slice($explode_url, 2);
+
             $request->controller = $explode_url[0];
 
-            if (empty($explode_url[1])) $request->action = "index";
-            else $request->action = $explode_url[1];
-
-            $p = array_slice($explode_url, 2);
-            // print_r($p);
-            $request->params = $p;
+            if (empty($explode_url[1])) {
+                $request->action = "index";
+            } else {
+                $request->action = $explode_url[1];
+            }
+            $request->params = array_slice($explode_url, 2);
         }
     }
 }
