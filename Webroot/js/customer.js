56c7cf946cd4bfde3c0696407c1796de10464f61
sanduni <dsanduni@icptechno.com>
2022-11-28 09:57:52 +0530

    feat: customer links

--- Webroot/js/customer.js
+++ Webroot/js/customer.js
@@ -1,3 +1,7 @@
-window.addEventListener('click', () => {
-    document.getElementById('sidebar_link').style.top = 0.2 * window.scrollY + 'px';
-})
+// function to colr active of sidebar in customer
+function activeSidebar() {
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+    var active = document.getElementById(page);
+    active.classList.add("active");
+}
