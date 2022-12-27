69962f349c65f794a6e2ebf241f80111a7998716
Janith Madarasinghe <janithpm9991@gmail.com>
2022-12-27 09:22:16 +0530

    homepage

--- Webroot/index.php
+++ Webroot/index.php
@@ -1,15 +1,14 @@
 <?php
 
-define('WEBROOT', str_replace("Webroot/index.php", "", $_SERVER["SCRIPT_NAME"]));
-define('ROOT', str_replace("Webroot/index.php", "", $_SERVER["SCRIPT_FILENAME"]));
+define('WEBROOT', str_replace("Webroot/index.php", "", $_SERVER["SCRIPT_NAME"])); // /govithena/
+define('ROOT', str_replace("Webroot/index.php", "", $_SERVER["SCRIPT_FILENAME"])); // C:/xampp/htdocs/govithena/
+define('URLROOT', 'http://localhost/govithena'); // http://localhost/govithena
 
-define('URLROOT', 'http://localhost/govithena');
-
-define('COMPONENTS', ROOT . 'Views/components/');
-define('IMAGES', URLROOT . '/Webroot/images');
-define('UPLOADS', URLROOT . '/Webroot/uploads/');
-define('CSS', URLROOT . '/Webroot/css');
-define('JS', URLROOT . '/Webroot/js');
+define('COMPONENTS', ROOT . 'Views/components/'); // C:/xampp/htdocs/govithena/Views/components/
+define('IMAGES', URLROOT . '/Webroot/images/'); // http://localhost/govithena/Webroot/images/
+define('UPLOADS', URLROOT . '/Webroot/uploads/'); // http://localhost/govithena/Webroot/uploads/
+define('CSS', URLROOT . '/Webroot/css/'); // http://localhost/govithena/Webroot/css/
+define('JS', URLROOT . '/Webroot/js/'); // http://localhost/govithena/Webroot/js/
 
 require(ROOT . 'Config/core.php');
 require(ROOT . 'dispatcher.php');
