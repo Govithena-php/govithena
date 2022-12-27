fdd7f24d58448bcba2ebf80158e7d071428d38c7
Janith Madarasinghe <janithpm9991@gmail.com>
2022-12-27 13:45:02 +0530

    feat: add prefix and suffix to uid

--- Models/user.php
+++ Models/user.php
@@ -14,6 +14,7 @@ class User extends Model
             return $farmer;
         } catch (PDOException $e) {
             echo $e->getMessage();
+            die();
             return null;
         }
     }
