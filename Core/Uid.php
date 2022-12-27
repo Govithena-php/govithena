98e150290e4f9b2a697202508ee295bdfc1c1f68
Janith Madarasinghe <janithpm9991@gmail.com>
2022-12-27 13:48:03 +0530

    add prefix and suffix to uid

--- Core/Uid.php
+++ Core/Uid.php
@@ -54,26 +54,4 @@ class UID
     {
         return $this->uid;
     }
-
-
-
-
-
-    // private $prefix;
-    // private $key;
-    // private $suffix;
-
-    // function __construct($type = "", $prefix = "", $suffix = "")
-    // {
-    //     if (empty($type))
-    //         $this->prefix = $prefix;
-    //     $this->suffix = $suffix;
-    //     $key = uniqid();
-    //     $this->key = $suffix . $key . $prefix;
-    // }
-
-    // public function get()
-    // {
-    //     return $this->key;
-    // }
 }
