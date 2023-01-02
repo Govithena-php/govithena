b9eeb8f12b2cad2d1e5ceb226f2589a502db174d
Janith Madarasinghe <janithpm9991@gmail.com>
2023-01-02 21:34:17 +0530

    service, title section added

--- Views/components/footer.php
+++ Views/components/footer.php
@@ -1,3 +1,57 @@
-<div class="[ footer bg-dark text-light ff-poppins fs-3  ]">
-    <p>GOVITHENA &copy; <?php echo date("Y"); ?></p>
-</div>
+<link rel="stylesheet" href="<?php echo CSS ?>/footer/footer.css">
+<link rel="stylesheet" href="<?php echo CSS ?>/grid.css">
+
+<footer>
+    <div class="[ container ]" container-type="footer">
+        <div class="[ grid ]" gap="2" md="2" lg="4">
+            <div class="[ box ]">
+                <h3>Categories</h3>
+                <ul>
+                    <li><a href="<?php echo URLROOT ?>/">Vegetables</a></li>
+                    <li><a href="<?php echo URLROOT ?>/">Fruits</a></li>
+                    <li><a href="<?php echo URLROOT ?>/">Grains</a></li>
+                    <li><a href="<?php echo URLROOT ?>/">Spices</a></li>
+                    <li><a href="<?php echo URLROOT ?>/">Rice</a></li>
+                </ul>
+            </div>
+            <div class="[ box ]">
+                <h3>About</h3>
+                <ul>
+                    <li><a href="<?php echo URLROOT ?>/">About us</a></li>
+                    <li><a href="<?php echo URLROOT ?>/">Our services</a></li>
+                    <li><a href="<?php echo URLROOT ?>/">Term and conditions</a></li>
+                    <li><a href="<?php echo URLROOT ?>/">privacy and policies</a></li>
+                </ul>
+            </div>
+            <div class="[ box ]">
+                <h3>Support</h3>
+                <ul>
+                    <li><a href="<?php echo URLROOT ?>/">Help center</a></li>
+                    <li><a href="<?php echo URLROOT ?>/">Trust and safety</a></li>
+                </ul>
+            </div>
+            <div class="[ box ]">
+                <h3>Site Map</h3>
+                <ul>
+                    <li><a href="<?php echo URLROOT ?>/">Dashboard</a></li>
+                    <li><a href="<?php echo URLROOT ?>/">Search</a></li>
+                    <li><a href="<?php echo URLROOT ?>/">About us</a></li>
+                    <li><a href="<?php echo URLROOT ?>/">Our services</a></li>
+                    <li><a href="<?php echo URLROOT ?>/">Contact us</a></li>
+                </ul>
+            </div>
+
+        </div>
+        <div class="[ branding ]">
+            <div class="[ header ]">
+                <a href="<?php echo URLROOT ?>">Govithena</a>
+                <p>&copy; <?php echo date("Y"); ?></p>
+            </div>
+            <div class="[ logo ]">
+                <a href="<?php echo URLROOT ?>">
+                    <img src="<?php echo IMAGES ?>/logo.svg" alt="logo">
+                </a>
+            </div>
+        </div>
+    </div>
+</footer>
