d367aeb9903311db01a1e8ab79eed8314014381d
Janith Madarasinghe <janithpm9991@gmail.com>
2023-01-07 14:18:43 +0530

    fix: sidebar routes

--- Views/dashboard/navigator.php
+++ Views/dashboard/navigator.php
@@ -47,25 +47,25 @@ function highlight($active, $link)
     <div class="[ links ]">
         <ul>
             <li>
-                <a href="<?php echo URLROOT ?>/investor/" class="<?php highlight($active, "dashboard") ?>">
+                <a href="<?php echo URLROOT ?>/dashboard/" class="<?php highlight($active, "dashboard") ?>">
                     <i class="fa-solid fa-gauge"></i>
                     <p>Dashboard</p>
                 </a>
             </li>
             <li>
-                <a href="<?php echo URLROOT ?>/investor/myinvestments" class="<?php highlight($active, "myinvestments") ?>">
+                <a href="<?php echo URLROOT ?>/dashboard/myinvestments" class="<?php highlight($active, "myinvestments") ?>">
                     <i class="[ fa-solid fa-sack-dollar ]"></i>
                     <p>My Investments</p>
                 </a>
             </li>
             <li>
-                <a href="<?php echo URLROOT ?>/investor/myfarmers" class="<?php highlight($active, "myfarmers") ?>">
+                <a href="<?php echo URLROOT ?>/dashboard/myfarmers" class="<?php highlight($active, "myfarmers") ?>">
                     <i class="[ fa-solid fa-tractor ]"></i>
                     <p>Farmers</p>
                 </a>
             </li>
             <li>
-                <a href="<?php echo URLROOT ?>/investor/myrequests" class="<?php highlight($active, "myrequests") ?>">
+                <a href="<?php echo URLROOT ?>/dashboard/myrequests" class="<?php highlight($active, "myrequests") ?>">
                     <i class="[ fa-solid fa-tractor ]"></i>
                     <p>My Request</p>
                 </a>
@@ -74,19 +74,19 @@ function highlight($active, $link)
         <div class="[ grow ]"></div>
         <ul>
             <li>
-                <a href="<?php echo URLROOT ?>/investor/myaccount" class="<?php highlight($active, "myaccount") ?>">
+                <a href="<?php echo URLROOT ?>/dashboard/myaccount" class="<?php highlight($active, "myaccount") ?>">
                     <i class="[ fa-solid fa-user-tie ]"></i>
                     <p>My Account</p>
                 </a>
             </li>
             <li>
-                <a href="<?php echo URLROOT ?>/investor/help" class="<?php highlight($active, "help") ?>">
+                <a href="<?php echo URLROOT ?>/dashboard/help" class="<?php highlight($active, "help") ?>">
                     <i class="[ fa-solid fa-circle-question ]"></i>
                     <p>Help</p>
                 </a>
             </li>
             <li>
-                <a href="<?php echo URLROOT ?>/investor/settings" class="<?php highlight($active, "settings") ?>">
+                <a href="<?php echo URLROOT ?>/dashboard/settings" class="<?php highlight($active, "settings") ?>">
                     <i class="[ fa-solid fa-gear ]"></i>
                     <p>Settings</p>
                 </a>
