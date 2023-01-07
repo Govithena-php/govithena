a2079b7ec0ad4f30ce14ecceb7b5a6304ac745df
Janith Madarasinghe <janithpm9991@gmail.com>
2023-01-07 14:11:05 +0530

    ui: table, footer, map, cards

---
+++ Views/investor/myfarmers.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="<?php echo IMAGES ?>/favicon.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+
+    <link rel="stylesheet" type="text/css" href="<?php echo CSS; ?>/base.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo CSS; ?>/investor/index.css">
+
+    <title>Dashboard | Investor</title>
+</head>
+
+<body>
+
+    <?php
+    $active = "myfarmers";
+    require_once("navigator.php");
+    ?>
+
+    <?php $name = "Janith"; ?>
+
+    <div class="[ container ][ cards ]" container-type="dashboard-section">
+        <div class="[ header ]">
+            <h1>myfarmers</h1>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+    </div>
+    <script src="<?php echo JS ?>/dashboard/dashboard.js"></script>
+</body>
+
+</html>
