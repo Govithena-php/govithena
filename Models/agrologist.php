06464f2a969b35e9e648891fc0840dff00d40bb8
sanduni <dsanduni@icptechno.com>
2023-01-08 14:28:25 +0530

    feat: agrologist dashboard sidebar, requests, myaccount, request details

---
+++ Models/agrologist.php
@@ -0,0 +1,62 @@
+<?php
+
+class Agrologist extends Model
+{
+    function farmerRequest()
+    {
+        try {
+            $sql = "SELECT CONCAT(u.firstName, ' ', u.lastName) AS fullName, u.city, a.requestId FROM agrologist_request a LEFT JOIN user u ON u.uid = a.farmerId WHERE (a.agrologistId = :agrologistId AND a.status='Pending')";
+            $stmt =  Database::getBdd()->prepare($sql);
+            $stmt->execute(['agrologistId' => Session::get('uid')]);
+            $req = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $req;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            die();
+            return null;
+        }
+    }
+
+    function acceptRequest($requestId)
+    {
+        try {
+            $sql = "UPDATE agrologist_request SET status = 'Accepted' WHERE requestId = :requestId";
+            $stmt = Database::getBdd()->prepare($sql);
+            $stmt->execute(['requestId' => $requestId]);
+            return true;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            die();
+            return null;
+        }
+    }
+
+    function getAgrologistDetails()
+    {
+        try {
+            $sql = "SELECT u.firstName, u.lastName, u.NIC, u.phoneNumber, u.city, c.userType, c.username FROM user u LEFT JOIN login_credential c ON u.uid = c.uid WHERE u.uid = :uid";
+            $stmt = Database::getBdd()->prepare($sql);
+            $stmt->execute(['uid' => Session::get('uid')]);
+            $req = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $req;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            die();
+            return null;
+        }
+    }
+
+    public function edit_user_details($data){
+        $sql = "UPDATE user SET firstName=:firstName, lastName=:lastName, phoneNumber=:phoneNumber, city=:city WHERE uid=:uid";
+        $req = Database::getBdd()->prepare($sql);
+        $req->execute([
+            'uid' => $data['uid'],
+            'firstName' => $data['firstName'],
+            'lastName' => $data['lastName'],
+            'city' => $data['city'],
+            'phoneNumber' => $data['phoneNumber'],
+        ]);
+        return $req->fetch();
+    }
+
+}
