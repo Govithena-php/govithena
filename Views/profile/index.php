fc944cd07d078bc6fdf084c2caa65d7ecf044928
Janith Madarasinghe <janithpm9991@gmail.com>
2023-01-24 21:41:56 +0530

    ui: profile

--- Views/profile/index.php
+++ Views/profile/index.php
@@ -30,15 +30,18 @@
                     <div class="[ user__details ]">
                         <h1>John Doe</h1>
                         <p>Farmer</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                         <p>vegetable</p>
-                        <ul>
-                            <li><i class="fa fa-phone"></li>
-                            <li><i class="fa fa-phone"></li>
+                        <ul class="[ flex-row ]">
+                            <li><i class="fa fa-phone"></i></li>
+                            <li><i class="fa fa-envelope"></i></li>
                         </ul>
                     </div>
                     <i class="fa fa-bookmark"></i>
                 </div>
+            </div>
+            <div>
+                <h2>Description</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non sapiente quis recusandae rerum, quo id molestias minima! Facilis molestiae facere optio natus beatae deserunt ratione et quo deleniti, consequatur assumenda.</p>
             </div>
         </div>
 
@@ -89,16 +92,208 @@
                 </li>
             </ul>
         </div>
-
     </div>
-
 
     <div class="[ container ]" type="section">
-        <h2>Description</h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non sapiente quis recusandae rerum, quo id molestias minima! Facilis molestiae facere optio natus beatae deserunt ratione et quo deleniti, consequatur assumenda.</p>
+        <h2> Active Gigs</h2>
+        <div class="[ my-2 ] [ grid ]" gap="1" md="2" lg="4">
+
+            <div class="[ result__card ]">
+                <div class="[ card__img ]">
+                    <img src="<?php echo IMAGES ?>temp/carrots.jpg" alt="test" />
+                    <!-- <img src="<?php echo IMAGES ?>/temp/17.jpg" alt="test"> -->
+                    <div class="[ farmer__name ]">
+                        <p><?php echo "firstname" ?></p>
+                        <p><?php echo "lastname" ?></p>
+                    </div>
+                </div>
+                <div class="[ card__content ]">
+                    <a class="[ text-dec-none mb-1 fs-5 text-dark fw-6 ]" href="<?php echo URLROOT . "/gig/" . $result['gigId'] ?>">
+                        <?php echo "title" ?>
+                    </a>
+                    <p class="[ sub-heading ]"><?php echo "capital" ?></p>
+                    <p><?php echo "location" ?></p>
+                    <div class="[ mt-1 flex flex-sb-c ]">
+                        <p><?php echo "category" ?></p>
+                        <p><?php echo "timeperiod" ?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="[ result__card ]">
+                <div class="[ card__img ]">
+                    <img src="<?php echo IMAGES ?>temp/banana.jpg" alt="test" />
+                    <!-- <img src="<?php echo IMAGES ?>/temp/17.jpg" alt="test"> -->
+                    <div class="[ farmer__name ]">
+                        <p><?php echo "firstname" ?></p>
+                        <p><?php echo "lastname" ?></p>
+                    </div>
+                </div>
+                <div class="[ card__content ]">
+                    <a class="[ text-dec-none mb-1 fs-5 text-dark fw-6 ]" href="<?php echo URLROOT . "/gig/" . $result['gigId'] ?>">
+                        <?php echo "title" ?>
+                    </a>
+                    <p class="[ sub-heading ]"><?php echo "capital" ?></p>
+                    <p><?php echo "location" ?></p>
+                    <div class="[ mt-1 flex flex-sb-c ]">
+                        <p><?php echo "category" ?></p>
+                        <p><?php echo "timeperiod" ?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="[ result__card ]">
+                <div class="[ card__img ]">
+                    <img src="<?php echo IMAGES ?>temp/radish.jpg" alt="test" />
+                    <!-- <img src="<?php echo IMAGES ?>/temp/17.jpg" alt="test"> -->
+                    <div class="[ farmer__name ]">
+                        <p><?php echo "firstname" ?></p>
+                        <p><?php echo "lastname" ?></p>
+                    </div>
+                </div>
+                <div class="[ card__content ]">
+                    <a class="[ text-dec-none mb-1 fs-5 text-dark fw-6 ]" href="<?php echo URLROOT . "/gig/" . $result['gigId'] ?>">
+                        <?php echo "title" ?>
+                    </a>
+                    <p class="[ sub-heading ]"><?php echo "capital" ?></p>
+                    <p><?php echo "location" ?></p>
+                    <div class="[ mt-1 flex flex-sb-c ]">
+                        <p><?php echo "category" ?></p>
+                        <p><?php echo "timeperiod" ?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="[ result__card ]">
+                <div class="[ card__img ]">
+                    <img src="<?php echo IMAGES ?>temp/radish.jpg" alt="test" />
+                    <!-- <img src="<?php echo IMAGES ?>/temp/17.jpg" alt="test"> -->
+                    <div class="[ farmer__name ]">
+                        <p><?php echo "firstname" ?></p>
+                        <p><?php echo "lastname" ?></p>
+                    </div>
+                </div>
+                <div class="[ card__content ]">
+                    <a class="[ text-dec-none mb-1 fs-5 text-dark fw-6 ]" href="<?php echo URLROOT . "/gig/" . $result['gigId'] ?>">
+                        <?php echo "title" ?>
+                    </a>
+                    <p class="[ sub-heading ]"><?php echo "capital" ?></p>
+                    <p><?php echo "location" ?></p>
+                    <div class="[ mt-1 flex flex-sb-c ]">
+                        <p><?php echo "category" ?></p>
+                        <p><?php echo "timeperiod" ?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="[ result__card ]">
+                <div class="[ card__img ]">
+                    <img src="<?php echo IMAGES ?>temp/radish.jpg" alt="test" />
+                    <!-- <img src="<?php echo IMAGES ?>/temp/17.jpg" alt="test"> -->
+                    <div class="[ farmer__name ]">
+                        <p><?php echo "firstname" ?></p>
+                        <p><?php echo "lastname" ?></p>
+                    </div>
+                </div>
+                <div class="[ card__content ]">
+                    <a class="[ text-dec-none mb-1 fs-5 text-dark fw-6 ]" href="<?php echo URLROOT . "/gig/" . $result['gigId'] ?>">
+                        <?php echo "title" ?>
+                    </a>
+                    <p class="[ sub-heading ]"><?php echo "capital" ?></p>
+                    <p><?php echo "location" ?></p>
+                    <div class="[ mt-1 flex flex-sb-c ]">
+                        <p><?php echo "category" ?></p>
+                        <p><?php echo "timeperiod" ?></p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
 
+    <div class="[ container ]" type="section">
+        <div class="[ rating__grid ]">
+            <div class="[ rating__number ]">
+                <h1>0.0</h1>
+                <div class="[ stars ]">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                </div>
+                <p>0 reviews</p>
+            </div>
+            <div class="[ rating__bars ]">
+                <div class="[ bar ]">
+                    <label for="5">5</label>
+                    <progress id="5" value="1" max="100"></progress>
+                </div>
+                <div class="[ bar ]">
+                    <label for="4">4</label>
+                    <progress id="4" value="1" max="100"></progress>
+                </div>
+                <div class="[ bar ]">
+                    <label for="3">3</label>
+                    <progress id="3" value="1" max="100"></progress>
+                </div>
+                <div class="[ bar ]">
+                    <label for="2">2</label>
+                    <progress id="2" value="1" max="100"></progress>
+                </div>
+                <div class="[ bar ]">
+                    <label for="1">1</label>
+                    <progress id="1" value="1" max="100"></progress>
+                </div>
+            </div>
+        </div>
 
+        <div class="[ reviews ]">
+            <h1>Reviews</h1>
+            <hr>
+            <div class="[ reviews__wrapper ]">
+
+                <div class="[ review ]">
+                    <div class="[ review__header ]">
+                        <img src="https://xsgames.co/randomusers/avatar.php?g=male" alt="profile">
+                        <h3>Reviewer name</h3>
+                    </div>
+                    <p>
+                        Occaecat occaecat et laborum exercitation eiusmod minim. Adipisicing consequat minim nostrud aliqua eu eu laborum officia. Deserunt ex qui consectetur Lorem excepteur culpa cillum culpa aute commodo velit est ex ut.
+                    </p>
+                    <div class="[ review__footer ]">
+                        <p>12/12/2022</p>
+                        <div class="[ stars ]">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="[ review ]">
+                    <div class="[ review__header ]">
+                        <img src="https://xsgames.co/randomusers/avatar.php?g=male" alt="profile">
+                        <h3>Reviewer name</h3>
+                    </div>
+                    <p>
+                        Occaecat occaecat et laborum exercitation eiusmod minim. Adipisicing consequat minim nostrud aliqua eu eu laborum officia. Deserunt ex qui consectetur Lorem excepteur culpa cillum culpa aute commodo velit est ex ut.
+                    </p>
+                    <div class="[ review__footer ]">
+                        <p>12/12/2022</p>
+                        <div class="[ stars ]">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <?php
+    require COMPONENTS . "footer.php";
+    ?>
     <script type="text/javascript" src="<?php echo JS ?>navbar/navbar.js"></script>
 </body>
 
