8caec3ed180dee5a35381398edf9e0c1742dcbbe
TharasaraWickramasinghe <88631210+TharasaraWickramasinghe@users.noreply.github.com>
2023-02-05 16:01:07 +0530

    Updated progress form and Technical assistant form

--- Views/farmer/techassistantfirst.php
+++ Views/farmer/techassistantfirst.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" type="text/css" href="<?php echo CSS; ?>/base.css">
     <link rel="stylesheet" type="text/css" href="<?php echo CSS; ?>/grid.css">
     <link rel="stylesheet" href="<?php echo CSS ?>/ui.css">
+    <link rel="stylesheet" href="<?php echo CSS ?>/search.css">
     
     <link rel="stylesheet" href="<?php echo CSS ?>/farmer/techassistantfirst.css">
 
@@ -28,7 +29,13 @@
     
 
     <div class="container" container-type="dashboard-section">
+           <!-- <form class="[ fs-3 ][ search__form ]" action="<?php echo URLROOT . "/search/" ?>" method="get">
+                <input class="" type="text" name="terms" placeholder="search by: name / location">
+                <button class="[ btn btn-primary ] [ search_button ]" type="submit">search</button>
+            </form> -->
+
         <div class="cardspace">
+  
              <select name="type" id="type">
                     <option value="croptype">Crop Type</option>                 
              </select>
@@ -44,7 +51,15 @@
              <select name="type" id="type">
                     <option value="location">Location</option>                 
              </select>
+             
+        
+             <form class="[ fs-3 ][ search__form ]" action="<?php echo URLROOT . "/search/" ?>" method="get">
+                <input class="" type="text" name="terms" placeholder="search by: name / location">
+                <button class="[ btn btn-primary ] [ search_button ]" type="submit">search</button>
+            </form>
+
         </div>
+
         <div class="cardspace">
         <a class="[ text-dec-none  text-dark  ]" href="<?php echo URLROOT . "/farmer/techassistant/" ?>">
 
@@ -57,7 +72,7 @@
                 <div class="cardbox">
                     <div class="techflex">
                        <p><i class="fa-solid fa-user"></i> Avishka Prabath </p>
-                        <p><i class="fa-solid fa-location-dot"></i> Matara, Matara </p>
+                        <p><i class="fa-solid fa-location-dot"></i> Anuradhapura, Rambewa </p>
                      </div>
                 </div>
             </div>
@@ -81,8 +96,8 @@
              <img class="techimg" src="<?php echo IMAGES ?>/techassistant/techassistant2.jpg" alt="profile">
                  <div class="cardbox">
                     <div class="techflex">
-                      <p><i class="fa-solid fa-user"></i> Avishka Prabath </p>
-                      <p><i class="fa-solid fa-location-dot"></i> Matara, Matara </p>
+                      <p><i class="fa-solid fa-user"></i> Pasindu Maduranga </p>
+                      <p><i class="fa-solid fa-location-dot"></i> polonnaruwa, Bubula </p>
                    </div>
               </div>
           </div>
@@ -92,6 +107,30 @@
             </div>
         </div>         
         </a>     
+
+
+
+
+        <a class="[ text-dec-none  text-dark  ]" href="<?php echo URLROOT . "/farmer/techassistant/" ?>">
+
+            <div class="cardpad">
+
+            <div class="cardtechfst">
+            <div class="cardimg">
+                <img class="techimg" src="<?php echo IMAGES ?>/techassistant/techassistant5.jpg" alt="profile">
+                    <div class="cardbox">
+                        <div class="techflex">
+                        <p><i class="fa-solid fa-user"></i> Dilshan Gamage </p>
+                        <p><i class="fa-solid fa-location-dot"></i> Matara, Matara </p>
+                    </div>
+                </div>
+            </div>
+
+                <p> LKR 50,000 - 75,000</p>
+                <p>This is gig tittle</p>
+                </div>
+            </div>         
+            </a>    
     
         </div>
 
@@ -110,8 +149,8 @@
                     <img class="techimg" src="<?php echo IMAGES ?>/techassistant/techassistant3.jpg" alt="profile">
                     <div class="cardbox">
                         <div class="techflex">
-                        <p><i class="fa-solid fa-user"></i> Avishka Prabath </p>
-                            <p><i class="fa-solid fa-location-dot"></i> Matara, Matara </p>
+                        <p><i class="fa-solid fa-user"></i> Dinesh Silva </p>
+                            <p><i class="fa-solid fa-location-dot"></i> Anuradhapura, Kudagama </p>
                         </div>
                     </div>
                 </div>
@@ -132,11 +171,11 @@
 
             <div class="cardtechfst">
             <div class="cardimg">
-                <img class="techimg" src="<?php echo IMAGES ?>/techassistant/techassistant4.jpg" alt="profile">
+                <img class="techimg" src="<?php echo IMAGES ?>/techassistant/techassistant6.jpg" alt="profile">
                     <div class="cardbox">
                         <div class="techflex">
-                        <p><i class="fa-solid fa-user"></i> Avishka Prabath </p>
-                        <p><i class="fa-solid fa-location-dot"></i> Matara, Matara </p>
+                        <p><i class="fa-solid fa-user"></i> Nadunn Sandeepa </p>
+                        <p><i class="fa-solid fa-location-dot"></i> Badulla, Ketawala </p>
                     </div>
                 </div>
             </div>
@@ -145,7 +184,32 @@
                 <p>This is gig tittle</p>
                 </div>
             </div>      
-             </a>        
+             </a>    
+             
+             
+
+
+             <!-- ---------------------- -->
+             <a class="[ text-dec-none  text-dark  ]" href="<?php echo URLROOT . "/farmer/techassistant/" ?>">
+
+                <div class="cardpad">
+
+                <div class="cardtechfst">
+                <div class="cardimg">
+                    <img class="techimg" src="<?php echo IMAGES ?>/techassistant/techassistant4.jpg" alt="profile">
+                        <div class="cardbox">
+                            <div class="techflex">
+                            <p><i class="fa-solid fa-user"></i> Harsha Ekanayake </p>
+                            <p><i class="fa-solid fa-location-dot"></i> Kurunegala, Bogoda </p>
+                        </div>
+                    </div>
+                </div>
+
+                    <p> LKR 50,000 - 75,000</p>
+                    <p>This is gig tittle</p>
+                    </div>
+                </div>         
+                </a>    
         </div>
 
 
