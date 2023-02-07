ca40d9b1badb1d392501d8cf774ccfe79fbd3e3d
Janith Madarasinghe <janithpm9991@gmail.com>
2023-02-07 16:04:42 +0530

    feat: access control

--- Controllers/techController.php
+++ Controllers/techController.php
@@ -2,27 +2,43 @@
 
 class techController extends Controller
 {
+    private $currentUser;
+
+    public function __construct()
+    {
+        $this->currentUser = Session::get('user');
+
+        if (!Session::isLoggedIn()) {
+            $this->redirect('/auth/signin');
+        }
+
+        if (!$this->currentUser->hasAccess(ACTOR::TECH_ASSISTANT)) {
+            $this->redirect('/error/dontHaveAccess');
+        }
+    }
 
     public function index()
     {
         $this->render('index');
     }
 
-    public function farmers(){
+    public function farmers()
+    {
         $this->render('farmers');
     }
 
-    public function requests(){
+    public function requests()
+    {
         $this->render('requests');
     }
 
-    public function farmerdetails(){
+    public function farmerdetails()
+    {
         $this->render('farmerdetails');
     }
 
-    public function myaccount(){
+    public function myaccount()
+    {
         $this->render('myaccount');
     }
-  
-
 }
