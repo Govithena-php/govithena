964b0220670c5ebacc3c6c0ae8e3fc21a0b7ecba
SavaniHasadara <91213642+SavaniHasadara@users.noreply.github.com>
2023-02-07 14:52:08 +0530

    new file:   Controllers/techController.php 	new file:   Views/tech/myaccount.php 	new file:   Views/tech/requests.php
     Untracked files:
    	Views/tech/farmerdetails.php
    	Views/tech/farmers.php
    	Views/tech/footer.php
    	Views/tech/index.php
    	Views/tech/navigator.php
    	Webroot/css/searchfarmer.css
    	Webroot/css/tech/
    	Webroot/css/techassistantrequests.css

---
+++ Controllers/techController.php
@@ -0,0 +1,28 @@
+<?php
+
+class techController extends Controller
+{
+
+    public function index()
+    {
+        $this->render('index');
+    }
+
+    public function farmers(){
+        $this->render('farmers');
+    }
+
+    public function requests(){
+        $this->render('requests');
+    }
+
+    public function farmerdetails(){
+        $this->render('farmerdetails');
+    }
+
+    public function myaccount(){
+        $this->render('myaccount');
+    }
+  
+
+}
