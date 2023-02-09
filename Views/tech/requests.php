ccec8e27b35e4f74f9fa9c59f5c0e59b48ea87fd
SavaniHasadara <91213642+SavaniHasadara@users.noreply.github.com>
2023-02-09 11:37:03 +0530

    feat: farmer requests

--- Views/tech/requests.php
+++ Views/tech/requests.php
@@ -7,10 +7,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="<?php echo IMAGES ?>/favicon.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
-
+    <link rel="stylesheet" href="../Webroot/css/ui.css">
     <link rel="stylesheet" type="text/css" href="<?php echo CSS; ?>/base.css">
     <link rel="stylesheet" type="text/css" href="<?php echo CSS; ?>/grid.css">
     <link rel="stylesheet" type="text/css" href="<?php echo CSS; ?>/table.css">
+    <link rel="stylesheet" href="<?php echo CSS ?>/agrologist/requests.css">
     <link rel="stylesheet" type="text/css" href="<?php echo CSS; ?>/tech/requests.css">
 
     <title>Dashboard | Tech Assistant</title>
@@ -25,44 +26,66 @@
     ?>
 
     <div class="[ container ][ ]" container-type="dashboard-section">
-        <div class="result_grid">
+        <h1 class="[ page-heading-1 ]">requests</h1>
+        <!-- <h1><?php echo Session::get('user')->getUid(); ?></h1> -->
+        <div class="[ requests__continer ]">
+            <?php
+            if (!isset($ar) || empty($ar)) {
+            ?>
+                <div class="[ no_requests__card ]">
+                    <p class=''>No Requests</p>
+                </div>
+            <?php
+            } else {
+            ?>
+                <div class="[ requests__wrapper ]">
+                    <?php
+                    foreach ($ar as $request) {
+                    ?>
 
-            <div class="result_card">  
-                <div class="profile_img">
-                    <img src="<?php echo IMAGES?>/21.jpg" alt=""/>
-                </div>
-                
-                <div class="card_content">
-                    <h4>Farmer Name</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut nemo expedita.</p>
-                    <p>5:26 pm</p>
-                </div>
-                
-                <div class="btn_group">
-                    <a href="" class="accept_btn">Accept</a>
-                    <a href="" class="ignore_btn">Ignore</a>
-                </div>
-            </div>
-            <div class="result_card">  
-                <div class="profile_img">
-                    <img src="<?php echo IMAGES?>/21.jpg" alt=""/>
-                </div>
-                
-                <div class="card_content">
-                    <h4>Farmer Name</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut nemo expedita.</p>
-                    <p>5:26 pm</p>
-                </div>
-                
-                <div class="btn_group">
-                    <a href="" class="accept_btn">Accept</a>
-                    <a href="" class="ignore_btn">Ignore</a>
-                </div>
-            </div>
+                        <div class="[ request__card bg-light ]">
+                            <div class="[ request__img ]">
+                                <img src="<?php echo IMAGES ?>/farmer.jpeg" alt="">
+                            </div>
+                            <form action="<?php echo URLROOT . '/tech/requests' ?>" method="POST">
+                                <div class="flex flex-row " style="width: 600px">
+                                    <div class="[ request__content ]">
 
+                                        <h1>
+                                            <a class="[ text-dec-none  text-dark  ]" href="<?php echo URLROOT . "/agrologist/requests/" . $request['requestId'] ?>">
+                                                <?php echo ucwords($request['fullName']) ?>
+                                            </a>
+                                        </h1>
+                                        <h4>
+                                            <?php echo ucwords($request['city']) ?>
+                                        </h4>
+                                        <p class="flex flex-row">
+                                            <span class="fa fa-star"></span>
+                                            <span class="fa fa-star"></span>
+                                            <span class="fa fa-star"></span>
+                                            <span class="fa fa-star"></span>
+                                            <span class="fa fa-star"></span>
+                                        </p>
+
+                                    </div>
+                                    <div class="flex flex-row flex-c-c">
+                                        <button type="submit" value="<?php echo $request['requestId'] ?> " class="btn btn-primary mr-2 mt-2" name="accept">Accept</button>
+                                        <button class="btn btn-danger mt-2" name="decline">Decline</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    <?php
+                    }
+                    ?>
+                </div>
+            <?php
+            }
+            ?>
         </div>
+
     </div>
-    
+
     <?php
     require_once("footer.php");
     ?>
