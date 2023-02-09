a0aef3d2198664717c6ab0876874e5ccc79781dc
Janith Madarasinghe <janithpm9991@gmail.com>
2023-02-09 14:16:59 +0530

    table name investment_gig > investment

--- Models/investment.php
+++ Models/investment.php
@@ -5,7 +5,7 @@ class Investment extends Model
     public function fetchAllBy($id)
     {
         try {
-            $sql = "SELECT * FROM investment_gig INNER JOIN gig ON investment_gig.gigId = gig.gigId WHERE investorId = :id ORDER BY timestamp DESC";
+            $sql = "SELECT * FROM investment INNER JOIN gig ON investment.gigId = gig.gigId WHERE investorId = :id ORDER BY timestamp DESC";
             $stmt = Database::getBdd()->prepare($sql);
             $stmt->execute(['id' => $id]);
             $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
