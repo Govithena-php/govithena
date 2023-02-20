62e8ef2932e7df1282a1400456ffdb08f8383d52
Janith Madarasinghe <janithpm9991@gmail.com>
2023-02-20 16:10:06 +0530

    feat: contact page ui done

--- Views/home/contact.php
+++ Views/home/contact.php
@@ -17,8 +17,38 @@
 
     <?php require_once(COMPONENTS . 'navbar.php'); ?>
 
-    <div class="[ container ][  ]" container-type="section">
-        <p>At Govithena, we are dedicated to providing our clients with innovative and profitable investment opportunities in the agriculture sector. Our team of experts leverages their deep knowledge of the industry and access to cutting-edge technologies to identify high-potential investment opportunities and help you make informed decisions. Whether you're looking to invest in farmland, agribusiness, or cutting-edge agriculture technologies, we have the expertise and resources to help you achieve your goals. Our portfolio of investments is diversified and balanced, ensuring that you are exposed to a range of opportunities across different stages of the agriculture value chain. Investing with Govithena, not only offers you the potential for strong financial returns, but also the satisfaction of knowing that you are contributing to the growth of a sustainable and responsible agriculture sector. We are committed to responsible investment practices and work closely with our partners to ensure that our investments promote sustainable agriculture practices, conserve natural resources, and benefit local communities. Join us today and help build a brighter future for agriculture and the world. Get in touch with our team to learn more about our agriculture investment opportunities and start building your portfolio.</p>
+    <div class="[ container ][ contact ]" container-type="section">
+        <h1>Contact Us</h1>
+        <p>If you have any inquiries, comments, or suggestions, please do not hesitate to contact us using the form below.<br>Our team will do their best to respond to you as soon as possible.</p>
+        <div class="[ grid ]" lg="2" gap="1">
+            <form order-lg="2">
+                <div class="[ row ]">
+                    <div class="[ form__group ]">
+                        <label for="name">Name</label>
+                        <input type="text" name="name" id="name">
+                    </div>
+                    <div class="[ form__group ]">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" id="email">
+                    </div>
+                </div>
+                <div class="[ form__group ]">
+                    <label for="subject">Subject</label>
+                    <input type="text" name="subject" id="subject">
+                </div>
+                <div class="[ form__group ]">
+                    <label for="message">Message</label>
+                    <textarea name="message" id="message" cols="30" rows="10"></textarea>
+                </div>
+                <div class="[ form__group ]">
+                    <button type="submit">Send</button>
+                </div>
+            </form>
+            <div class="[ help ]" order-lg="1">
+                <img src="<?php echo IMAGES ?>/svg/contact_us.svg" alt="contact us" />
+                <p>In the meantime, if you are looking for answers to common questions or need assistance with our products or services, <br>we invite you to check out our <a href="">Help and Support Center.</a></p>
+            </div>
+        </div>
     </div>
 
     <?php require_once(COMPONENTS . 'footer.php'); ?>
