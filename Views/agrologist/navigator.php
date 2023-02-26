13f6f81d1bf8d4a881e0157ceaa680b229689e00
sandunialokah <msandunim@gmail.com>
2023-02-26 16:16:02 +0530

    feat: notifications backend and frontend integration

--- Views/agrologist/navigator.php
+++ Views/agrologist/navigator.php
@@ -1,16 +1,16 @@
 <?php
 
 $currentUser = Session::get('user');
-$d = json_decode(file_get_contents('php://input'), true);
+// $d = json_decode(file_get_contents('php://input'), true);
 
-if (empty(file_get_contents('php://input'))) {
-    echo "<h1 style='color: black; margin-top: 500px; margin-left: 1000px'>". 'no data found' . "</h1>";
-}
-$name = $d['name'];
-$n = $_POST['data'];
-$age = $d['age'];
-$greeting = "Hello, $n! You are $age years old.";
-//echo "<h1 style='color: black; margin-top: 500px; margin-left: 1000px'>". $name . "</h1>";
+// if (empty(file_get_contents('php://input'))) {
+//     echo "<h1 style='color: black; margin-top: 500px; margin-left: 1000px'>". 'no data found' . "</h1>";
+// }
+// $name = $d['name'];
+// $n = $_POST['datadata'];
+// $age = $d['age'];
+// $greeting = "Hello, $n! You are $age years old.";
+//echo "<h1 style='color: black; margin-top: 500px; margin-left: 1000px'>". $datadata[0]['message'] . "</h1>";
 
 //echo "<h1 style='color: white'>$name</h1>";
 
@@ -39,7 +39,7 @@ function highlight($active, $link)
                 <p>Govithena</p>
             </a>
         </div>
-       // <?php echo "<h1 style='color: white'>$name</h1>"; ?>
+       <!-- <?php echo "<h1 style='color: white'>$name</h1>"; ?> -->
         <div class="[ open__btn ]">
             <button onclick="toggleSidebar()">
                 <i class="fa-solid fa-bars"></i>
@@ -85,14 +85,22 @@ function highlight($active, $link)
                 </div>
 
 
-                <div id="notification_menu" open="false" class="[ menu ]">
-                    <div class="[ profile__name ]">
-                        <h3>
-                            <?php echo "<h1>$greeting</h1>"; ?>
-                        </h3>
-                        <small>
-                            <!-- <?php echo $currentUser->getType() ?> -->
-                        </small>
+                <div id="notification_menu" open="false" class="menu notification_menu ">
+                    <div class="[ notification_message ]">
+                        <?php 
+                        foreach ($notifications as $notification) {
+                            ?>
+                        <a>
+                            <?php echo $notification['message']; ?>
+                            
+                        </a>
+                        <hr>
+                        <?php
+                        }
+                        ?>
+                        <!-- <small>
+                            <?php echo $currentUser->getType() ?>
+                        </small> -->
                     </div>
                     <!-- <ul>
                         <li><a onclick="toggleNotificationMenu()" href="<?php echo URLROOT ?>/dashboard/">
