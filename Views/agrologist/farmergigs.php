f28382b1b67065e59eaaefa42e781616ed53b774
sandunialokah <msandunim@gmail.com>
2023-04-10 14:15:00 +0530

    feat: search farmergigs

--- Views/agrologist/farmergigs.php
+++ Views/agrologist/farmergigs.php
@@ -10,8 +10,7 @@
     <link rel="stylesheet" type="text/css" href="<?php echo CSS; ?>/base.css">
     <link rel="stylesheet" href="<?php echo CSS ?>/ui.css">
     <link rel="stylesheet" href="<?php echo CSS ?>/grid.css">
-    <link rel="stylesheet" href="<?php echo CSS ?>/search.css">
-    
+    <link rel="stylesheet" href="<?php echo CSS ?>/agrologist/farmergigs.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 </head>
 
@@ -22,7 +21,15 @@
     ?>
     <div class="[ container ][ dashboard ]" container-type="dashboard-section">
         <h1 class="[ page-heading-1 ]"><?php echo $gigDetails[0]['firstName'] . " " . $gigDetails[0]['lastName'] ?></h1>
+        
         <!-- <?php print_r($gigDetails) ?> -->
+
+        <div class="search">
+                <input type="text" placeholder="Search by: location/ category/ title/ time period" oninput="liveSearch()" id="searchbox">
+                <button type="button">
+                    <i class="fas fa-search"></i>
+                </button>
+            </div>
 
         <div class="[ my-2 ] [ grid ]" gap="1" md="2" lg="4">
             <?php
@@ -36,21 +43,21 @@
                     // die();
             
                     ?>
-                    <div class="[ result__card ]">
-                        <div class="[ card__img ]">
+                    <div class="result__card">
+                        <div class="card__img">
 
                             <img src="<?php echo $imageURL ?>" alt="test" />
 
                             <!-- <img src="<?php echo IMAGES ?>/temp/17.jpg" alt="test"> -->
-                            <div class="[ farmer__name ]">
+                            <div class="location_name">
 
                                 <p class="ml-1">
                                     <?php echo ucwords($gigDetail['location']) ?>
                                 </p>
                             </div>
                         </div>
-                        <div class="[ card__content ]">
-                            <a class="[ text-dec-none mb-1 fs-5 text-dark fw-6 ]" href="<?php echo URLROOT . "/agrologist/farmers/" . $gigDetail['farmerId'] . "/" . $gigDetail['gigId'] ?>">
+                        <div class="card__content">
+                            <a class="text-dec-none mb-1 fs-5 text-dark fw-6" href="<?php echo URLROOT . "/agrologist/farmers/" . $gigDetail['farmerId'] . "/" . $gigDetail['gigId'] ?>">
                                 <?php echo ucwords($gigDetail['title']) ?>
                             </a>
                             <div class="[ mt-1 flex flex-sb-c ]">
@@ -58,7 +65,7 @@
                                     <?php echo ucwords($gigDetail['category']) ?>
                                 </p>
                                 <p>
-                                    <?php echo $gigDetail['timePeriod'] ?>
+                                    <?php echo $gigDetail['timePeriod'] ?> days
                                 </p>
                             </div>
                         </div>
@@ -74,6 +81,24 @@
 
     <?php require "footer.php"; ?>
     <script src="<?php echo JS ?>/app.js"></script>
+    <script>
+
+        function liveSearch() {
+            let cards = document.querySelectorAll('.result__card')
+            let search_query = document.getElementById("searchbox").value;
+            for (var i = 0; i < cards.length; i++) {
+
+                if (cards[i].innerText.toLowerCase()
+                    .includes(search_query.toLowerCase())) {
+                    cards[i].classList.remove("is-hidden");
+                } else {
+                    cards[i].classList.add("is-hidden");
+                }
+            }
+        }
+
+    </script>
+
 
 </body>
 
