98b144ae95b37353c2e67104e07ee3484e8839c8
sandunialokah <msandunim@gmail.com>
2023-04-12 22:04:29 +0530

    feat: payments ui

--- Views/agrologist/payments.php
+++ Views/agrologist/payments.php
@@ -12,6 +12,8 @@
     <link rel="stylesheet" type="text/css" href="<?php echo CSS; ?>/grid.css">
     <link rel="stylesheet" href="../Webroot/css/agrologist/myaccount.css">
     <link rel="stylesheet" type="text/css" href="<?php echo CSS; ?>/agrologist/payments.css">
+    <link rel="stylesheet" href="<?php echo CSS ?>/investor/myinvestments.css">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 </head>
 
@@ -27,15 +29,40 @@
         <div class="content ff-poppins" style="background-color: white; margin-bottom: 100px; border-radius: 0.5rem;">
             <div class="p-2">
 
-                            <div class="fw-5">Days Remaining:   <span style="color: gray">   20 days</span></div><br>
-                            <div class="fw-5">No. of feild visits:   <span style="color: gray">  5</span></div>
-                            
-                    
+                <div class="fw-5">Total no. of days: <span style="color: gray"> 60 days</span></div>
+                <div class="fw-5 pt-1">Days Remaining: <span style="color: gray"> 20 days</span></div>
+                <div class="fw-5 pt-1">No. of feild visits: <span style="color: gray"> 5</span></div>
+                <div class="fw-5 pt-1">Total receivable: <span style="color: gray"> LKR 150000</span></div>
+                <div class="fw-5 pt-1">Total withdrawn: <span style="color: gray"> LKR 50000</span></div>
+
+
+                <div class="payment_container pt-2">
+                    <div class="payment_heading">
+                        <!-- <h3>Title</h3> -->
+                        <h3>Timestamp</h3>
+                        <h3>Amount</h3>
+                        <!-- <h3>Category</h3> -->
+                    </div>
+
+                    <div class="payment">
+                        <!-- <h3>title</h3> -->
+                        <p>timestamp</p>
+                        <p>amount</p>
+                        <!-- <p>category</p> -->
+                    </div>
+                    <div class="payment">
+                        <!-- <h3>title</h3> -->
+                        <p></p>
+                        <p>total recived</p>
+                        <!-- <p>category</p> -->
+                    </div>
                 </div>
 
             </div>
 
         </div>
+
+    </div>
     </div>
 
 
