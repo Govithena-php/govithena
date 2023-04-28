840f15ba8e3b76fbd499b85a16ef132c792e2168
TharasaraWickramasinghe <88631210+TharasaraWickramasinghe@users.noreply.github.com>
2023-04-28 21:09:50 +0530

    Investment requests

--- Views/farmer/investors.php
+++ Views/farmer/investors.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" type="text/css" href="<?php echo CSS; ?>/base.css">
     <link rel="stylesheet" type="text/css" href="<?php echo CSS; ?>/grid.css">
     <link rel="stylesheet" type="text/css" href="<?php echo CSS; ?>/table.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo CSS; ?>/gridTable.css">
     <link rel="stylesheet" href="<?php echo CSS ?>/ui.css">
 
     <!-- css file eka -->
@@ -149,11 +150,8 @@
             require(COMPONENTS . "dashboard/noDataFound.php");
         } else { ?>
             <div class="investors">
-                <?php
-                foreach ($investors as $investor) {
-                    $imgURL = UPLOADS . "/" . $investor['thumbnail'];
-                ?>
-                    <div class="investor__card">
+           
+                    <!-- <div class="investor__card">
                         <div class="card__img">
                             <img src="<?php echo $imgURL ?>" alt="">
                         </div>
@@ -181,11 +179,101 @@
                             </div>
                         </div>
 
+                    </div> -->
+
+
+
+
+
+                    <div class="grid__table"
+                        style="
+                                --xl-cols: 1.3fr 0.7fr 0.7fr 0.5fr 1.7fr 1fr;
+                                --lg-cols: 1.5fr 1fr 1fr 1fr 0.3fr;
+                                --md-cols: 2fr 1fr 0.3fr;
+                                --sm-cols: 3fr 0.3fr;
+                            "
+                        >
+                        <div class="head">
+                            <div class="row">
+                                <div class="data">
+                                    <p>Farmer Name</p>
+                                </div>
+                                <div class="data">
+                                    <p>title</p>
+                                </div>
+                                <div class="data">
+                                    <p>Offer</p>
+                                </div>
+                                <div class="data">
+                                    <p>Location</p>
+                                </div>
+                                <div class="data">
+                                    <p>Message</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="body">
+                            <?php
+                            foreach($investors as $investor){
+                                ?>
+                            <div class="row">
+                                <div class="data farmer__">
+                                    <div class="investorimg">
+                                    <img src="<?php echo UPLOADS . "/" . $investor['thumbnail']; ?>" alt="">
+                                        <!-- <img src="<?php echo UPLOADS . '/profilePictures/' . $investor['thumbnail']?>" alt="Picture"> -->
+                                    </div>
+                                    <div class="namecol">
+                                        <h1><p><?php echo $investor['firstName'] ?></p></h1>
+                                        <h3><p><?php echo $investor['lastName']?></p></h3>
+                                    </div>
+                                </div>
+                                <!-- <div class="data">
+                                      <p> LKR <?php echo $investor['offer'] ?></p>
+                                    <p class="LKR"><?php echo number_format($investor['capital'], 2, '.', ',')?></p>
+                                </div> -->
+                                <div class="data">
+                                    <p><?php echo $investor['title'] ?></p>
+                                </div>
+                                <div class="data">
+                                    <p> LKR <?php echo $investor['offer']?></p>
+                                </div>
+                                <div class="data">
+                                    <p><?php echo $investor['city']?></p>
+                                </div>
+                                <div class="data">
+                                    <p><?php echo $investor['message']?></p>
+                                </div>
+                                <div class="data">
+                                    <div class="actions">
+                                        <!-- <a href="#" class="btn btn-primary">Edit</a> -->
+                                        <button onclick="openAcceptModal('<?php echo $investor['requestId']?>')" class="button__primary">Accept</button>
+                                        <button onclick="openRejectModal('<?php echo $investor['requestId']?>')" class="button__danger">Reject</button>
+                                        <!-- <a href="<?php echo URLROOT . "/farmer/deleteGig/" . $investor['gigId'] ?>" class="btn btn-danger">Delete</a> -->
+                                    </div>
+                                </div>
+                            </div>
+                            <?php
+                            }
+                            ?>
+                        </div>
                     </div>
-                <?php
-                }
-                ?>
+       
             </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+             
+
         <?php
         }
         ?>
@@ -310,53 +398,7 @@
 
 
 
-               <div class="[ requests__cn ]">
-
-
-                <div class="[ requests__wrap ]">
-
-
-                        <div class="[ requestcard bg-light ]">
-
-                           <div class="[ requestimg ]">
-                                <img class="img" src="<?php echo IMAGES ?>/investor.jfif" alt="profile">
-                            </div>
-
-                    <form action="<?php echo URLROOT . '/agrologist/requests' ?>">
-                        <div class="flex flex-row ">
-                            <div class="[ requestcont ]">
-
-
-
-                                <h1>
-                                    <p><b>Amal Perera</b></p>
-                                </h1>
-                                <p>Sent you a request</p>
-
-
-
-                                <p class="flex flex-row">
-                                    <span class="fa fa-star"></span>
-                                    <span class="fa fa-star"></span>
-                                    <span class="fa fa-star"></span>
-                                    <span class="fa fa-star"></span>
-                                    <span class="fa fa-star"></span>
-                                </p>
-
-                            </div>
-
-                        </div>
-                    </form>
-
-                    <div class=" flex-c-c">
-                        <button type="button" class="btn_accept" name="accept">Accept</button> &emsp;
-                        <button type="button" class="btn_decline" name="decline">Decline</button>
-                    </div>
-
-                </div>
-
-            </div>
-        </div>
+        
 
     </div>
     <?php
